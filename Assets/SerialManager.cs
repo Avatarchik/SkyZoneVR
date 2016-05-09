95434d61ba4adb08ef2d99606425678e0fd1dbf4
Wyatt Bushnell <wyatt@virsix.com>
2016-05-09 15:06:36 -0700

    init serial push

---
+++ Assets/SerialManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+ 
+public class SerialManager : MonoBehaviour {
+  public string port;
+  private Thread thread;
+  SerialPort stream;// = new SerialPort("/dev/tty.usbmodem1411", 115200); 
+
+  int baudRate = 57600;
+  int readTimeout = 20;
+
+  private List<string> packetQueue = new List<string>();
+	
+  void Start () {
+    try {
+      stream = new SerialPort(port, baudRate);
+      stream.Open(); //Open the Serial Stream.
+      stream.ReadTimeout = readTimeout;
+
+      thread = new Thread (new ThreadStart (readSerial));
+      thread.Start();
+    } catch (Exception e) {
+      Debug.Log (e.Message);
+    }
+  }
+ 
+  public void Update (){
+    lock (packetQueue) {
+      foreach (string message in packetQueue) {
+        BroadcastMessage ("SerialInputRecieved", message, SendMessageOptions.DontRequireReceiver);
+	  }
+      packetQueue.Clear ();
+    }
+  }
+
+  private void readSerial(){
+    while(stream.IsOpen) {
+      try{
+        string lineToRead = stream.ReadLine(); 
+        if (lineToRead != null) {
+          lock (packetQueue) {
+            packetQueue.Add(lineToRead);
+          }
+        }
+        stream.BaseStream.Flush(); 
+      } catch (Exception e) { 
+        Debug.Log (e.Message);
+        Console.WriteLine (e.Message); 
+      }
+    }
+  }
+}
