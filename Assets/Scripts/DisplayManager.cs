4a8f5cf4fa8da4a2a36a91390121ded6f653d6c5
Greg Sotolov <greg@virsix.com>
2016-05-11 16:44:11 -0700

    Added tutorial video (will play/loop on start, but only if there is more than 1 display)

--- Assets/Scripts/DisplayManager.cs
+++ Assets/Scripts/DisplayManager.cs
@@ -7,10 +7,21 @@ public class DisplayManager : MonoBehaviour
 	public Camera standbyCamera;
 	public MovieTexture standbyVideoTexture;
 
+	public Camera tutorialCamera;
+	public MovieTexture tutorialVideo;
+
 	// Use this for initialization
 	void Start () 
 	{
-	
+		if (Display.displays.Length <= 1) {
+			print ("NOT ENOUGH DISPLAYS TO PLAY TUTORIAL VIDEO");
+			return;
+		}
+
+		if (!tutorialCamera.isActiveAndEnabled)
+			tutorialCamera.enabled = true;
+		tutorialVideo.Play ();
+		tutorialVideo.loop = true;
 	}
 	
 	// Update is called once per frame
