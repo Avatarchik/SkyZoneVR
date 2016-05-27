cb5514cb0a36abdc5f54aabce839cefbce53949d
Greg Sotolov <greg@virsix.com>
2016-05-26 18:03:10 -0700

    Fixed UI "smearing" problem

---
+++ Assets/Scripts/UI_Tracking.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Tracking : MonoBehaviour 
+{
+
+	public Transform playerHeadTransform;
+	public Transform uiTrackingTransform;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		transform.position = uiTrackingTransform.position;
+		transform.rotation = Quaternion.LookRotation((playerHeadTransform.position - uiTrackingTransform.position) * -1);
+	}
+}
